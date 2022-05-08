9f071f3a9256aedab5acf0007634f56ff84dab9d
ACGaming <4818419+ACGaming@users.noreply.github.com>
2022-05-08 10:47:48 +0200

    Fix tile entity registration

--- src/main/java/com/lumintorious/tfc_drying_rack/registry/TileEntityRegistry.java
+++ src/main/java/com/lumintorious/tfc_drying_rack/registry/TileEntityRegistry.java
@@ -1,13 +1,15 @@
 package com.lumintorious.tfc_drying_rack.registry;
 
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
+import com.lumintorious.tfc_drying_rack.TFCDryingRack;
 import com.lumintorious.tfc_drying_rack.objects.rack.TileEntityRack;
 
 public class TileEntityRegistry
 {
     public static void register()
     {
-        GameRegistry.registerTileEntity(TileEntityRack.class, TileEntityRack.NAME);
+        GameRegistry.registerTileEntity(TileEntityRack.class, new ResourceLocation(TFCDryingRack.MODID, TileEntityRack.NAME));
     }
 }
